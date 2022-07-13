2a11c4250b440d59851d47f3d471ac4fd1ecf764
Dominik Burgdörfer <db@burgdoerfer.com>
2022-07-13 15:47:29 +0200

    test: improve tests of optional test 6.2.14

--- tests/optionalTest_6_2_14.js
+++ tests/optionalTest_6_2_14.js
@@ -3,7 +3,11 @@ import { optionalTest_6_2_14 } from '../optionalTests.js'
 import readExampleFiles from './shared/readExampleFiles.js'
 
 const failingExamples = await readExampleFiles(
-  new URL('optionalTest_6_2_14', import.meta.url)
+  new URL('optionalTest_6_2_14/failing', import.meta.url)
+)
+
+const validExamples = await readExampleFiles(
+  new URL('optionalTest_6_2_14/valid', import.meta.url)
 )
 
 describe('Optional test 6.2.14', function () {
@@ -12,7 +16,17 @@ describe('Optional test 6.2.14', function () {
       it(title, async function () {
         const result = await optionalTest_6_2_14(failingExample)
 
-        expect(result.warnings).to.haveOwnProperty('length', 1)
+        expect(result.warnings).to.have.length.greaterThan(0)
+      })
+    }
+  })
+
+  describe('valid examples', function () {
+    for (const [title, validExample] of validExamples) {
+      it(title, async function () {
+        const result = await optionalTest_6_2_14(validExample)
+
+        expect(result.warnings).to.haveOwnProperty('length', 0)
       })
     }
   })
