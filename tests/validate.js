7b2416a9bb186691fbad37a730a7637a4d1c4692
Christoph Löser <33527573+cloeser@users.noreply.github.com>
2022-12-16 16:33:35 +0100

    refactor: formatting

--- tests/validate.js
+++ tests/validate.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import validateStrict from "../validateStrict.js";
+import validateStrict from '../validateStrict.js'
 
 describe('validateStrict', function () {
   it('throws if an unknown test function is passed and strict mode is used', async function () {
@@ -14,7 +14,9 @@ describe('validateStrict', function () {
       )
       expect.fail()
     } catch (/** @type {any} */ e) {
-      expect(e.message).to.contain('Execution of test functions not defined in the library is prohibited.')
+      expect(e.message).to.contain(
+        'Execution of test functions not defined in the library is prohibited.'
+      )
     }
   })
 })
