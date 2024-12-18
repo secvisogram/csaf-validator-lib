64ae0a843da0637f0f586149a721745e0b6aa57d
Marco Korinth <marco@bizfactory.tech>
2024-12-18 17:58:31 +0100

    refactor: module exports

---
+++ csaf-validator-lib.js
@@ -0,0 +1,6 @@
+export * as tests from './lib/tests.js'
+export * as cwe from './lib/cwe.js'
+export { default as getHunspellAvailableLangs } from './hunspell/getHunspellAvailableLangs.js'
+export { default as strip } from './lib/strip.js'
+export { default as validate } from './lib/validate.js'
+export { default as validateStrict } from './lib/validateStrict.js'
