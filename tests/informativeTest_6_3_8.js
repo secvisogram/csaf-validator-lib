11779f4fc7aead063b7141a08a8927e3ae3c2110
rschneider <97682836+rainer-exxcellent@users.noreply.github.com>
2025-02-07 11:53:08 +0100

    fix(performance): #201 segment text before call to hunspell and cache results

--- tests/informativeTest_6_3_8.js
+++ tests/informativeTest_6_3_8.js
@@ -1,128 +1,57 @@
 import { expect } from 'chai'
 import informativeTest_6_3_8 from '../lib/informativeTests/informativeTest_6_3_8.js'
-import minimalDoc from './shared/shared/minimalDoc.js'
-import csaf_2_0 from '../lib/schemaTests/csaf_2_0.js'
+import readExampleFiles from './shared/readExampleFiles.js'
 
-const validMarker = 'Mocked as Valid'
+const failingExamples = await readExampleFiles(
+  new URL('informativeTest_6_3_8/failing', import.meta.url)
+)
 
-const documentValidBase = {
-  document: {
-    ...minimalDoc.document,
-    notes: [
-      {
-        category: 'summary',
-        text: validMarker,
-      },
-    ],
-    publisher: {
-      category: 'other',
-      name: 'Example PUB ' + validMarker,
-      namespace: 'https://example.com',
-    },
-    references: [
-      {
-        category: 'self',
-        summary: 'A non-canonical URL. ' + validMarker,
-        url: 'https://example.com/security/data/csaf/2021/my-thing-_10.json',
-      },
-    ],
-    title: 'Title ' + validMarker,
-    tracking: {
-      ...minimalDoc.document.tracking,
-      revision_history: [
-        {
-          number: '1',
-          date: '2021-01-14T00:00:00.000Z',
-          summary: 'Summary ' + validMarker,
-        },
-      ],
-    },
-  },
-}
+// Word muss match to test csaf/csaf_2.0/test/validator/data/informative/oasis_csaf_tc-csaf_2_0-2021-6-3-08-11.json
+// and csaf/csaf_2.0/test/validator/data/informative/oasis_csaf_tc-csaf_2_0-2021-6-3-08-01.json
 
-/**
- * @param {object} params
- * @param {string} params.input
- * @returns
- */
-async function runHunspellMock({ input }) {
-  if (input.includes(validMarker)) {
-    return 'Hunspell vMOCK\n\n*'
-  } else {
-    return 'Hunspell vMOCK\n\n# wrongword 1'
-  }
-}
+const hunspellMap = new Map([
+  ['Security', '*'],
+  ['researchers', '*'],
+  ['found', '*'],
+  ['multiple', '*'],
+  ['vulnerabilities', '*'],
+  ['in', '*'],
+  ['XYZ', '*'],
+  ['Secruity', '# error'],
+  ['OASIS', '*'],
+  ['CSAF', '*'],
+  ['TC', '*'],
+  ['Informative', '*'],
+  ['test', '*'],
+  ['Spell', '*'],
+  ['check', '*'],
+  ['valid', '*'],
+  ['example', '*'],
+  ['failing', '*'],
+  ['1', '*'],
+  ['Initial', '*'],
+  ['version', '*'],
+  ['1', '*'],
+  ['1', '*'],
+])
 
 describe('Informative test 6.3.8', function () {
-  const csafWithInvalidTitle = {
-    document: {
-      ...documentValidBase.document,
-      title: 'Mock as invalid',
-    },
-  }
-
-  const csafWithInvalidProductName = {
-    document: {
-      ...documentValidBase.document,
-    },
-    product_tree: {
-      branches: [
-        {
-          branches: [
-            {
-              branches: [
-                {
-                  category: 'product_name',
-                  name: 'Mocked as Invalid',
-                  product: {
-                    name: validMarker,
-                    product_id: '7Client-7.6',
-                    product_identification_helper: {
-                      cpe: 'cpe:/o:redhat:enterprise_linux:7::client',
-                    },
-                  },
-                },
-              ],
-              category: 'product_family',
-              name: validMarker,
-            },
-          ],
-          category: 'vendor',
-          name: validMarker,
-        },
-      ],
-    },
-  }
-
   describe('failing examples', function () {
-    it('test invalid title', async function () {
-      expect(csaf_2_0(csafWithInvalidTitle).isValid).to.true
-      const result = await informativeTest_6_3_8(csafWithInvalidTitle, {
-        hunspell: runHunspellMock,
-      })
-      expect(result.infos.length).to.equal(1)
-      expect(result.infos[0].instancePath).to.equal('/document/title')
-    })
+    for (const [title, failingExample] of failingExamples) {
+      it(title, async function () {
+        const result = await informativeTest_6_3_8(failingExample, {
+          async hunspell({ dictionary, input }) {
+            const answer = hunspellMap.get(input)
+            if (answer) {
+              return 'Hunspell vMOCK\n\n' + answer
+            } else {
+              throw new Error('Hunspell vMOCK. Unknoww word ' + input)
+            }
+          },
+        })
 
-    it('test invalid product name in  branch', async function () {
-      expect(csaf_2_0(csafWithInvalidProductName).isValid).to.true
-      const result = await informativeTest_6_3_8(csafWithInvalidProductName, {
-        hunspell: runHunspellMock,
+        expect(result.infos).to.have.length.greaterThan(0)
       })
-      expect(result.infos.length).to.equal(1)
-      expect(result.infos[0].instancePath).to.equal(
-        '/product_tree/branches/0/branches/0/branches/0/name'
-      )
-    })
-  })
-
-  describe('valid examples', function () {
-    it('test valid csaf', async function () {
-      expect(csaf_2_0(documentValidBase).isValid).to.true
-      const result = await informativeTest_6_3_8(documentValidBase, {
-        hunspell: runHunspellMock,
-      })
-      expect(result.infos.length).to.equal(0)
-    })
+    }
   })
 })
