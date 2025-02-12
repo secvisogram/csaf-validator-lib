a5e960476d75ad71acfd54115a70a4819fdc170c
Dominik Burgdörfer <dominik.burgdoerfer@exxcellent.de>
2025-02-12 15:29:54 +0100

    feat: setup csaf 2.1 infrastructure

---
+++ scripts/mocha.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import { spawn } from 'child_process'
+import { fileURLToPath } from 'url'
+
+spawn(
+  'mocha',
+  ['--exclude', '**/*.test.js', 'tests', ...process.argv.slice(2)],
+  {
+    stdio: 'inherit',
+    env: {
+      ...process.env,
+      DICPATH: fileURLToPath(new URL('../tests/dicts', import.meta.url)),
+      WORDLIST: fileURLToPath(
+        new URL('../tests/dicts/csaf_words.txt', import.meta.url)
+      ),
+    },
+  }
+)
