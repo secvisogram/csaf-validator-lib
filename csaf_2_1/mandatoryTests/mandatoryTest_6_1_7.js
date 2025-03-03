379708963a08edd8f1f7236f1f1ca01d3a823bc1
rschneider <97682836+rainer-exxcellent@users.noreply.github.com>
2025-03-03 08:21:14 +0100

    feat(CSAF2.1): #197 replace preconditionFor_6_1_7_Matches with schema test

--- csaf_2_1/mandatoryTests/mandatoryTest_6_1_7.js
+++ csaf_2_1/mandatoryTests/mandatoryTest_6_1_7.js
@@ -1,100 +1,110 @@
+import Ajv from 'ajv/dist/jtd.js'
+
+const jtdAjv = new Ajv()
+
+const inputSchema = /** @type {const} */ ({
+  additionalProperties: true,
+  properties: {
+    vulnerabilities: {
+      elements: {
+        additionalProperties: true,
+        properties: {
+          metrics: {
+            elements: {
+              additionalProperties: true,
+              optionalProperties: {
+                cvss_v2: {
+                  additionalProperties: true,
+                  properties: {
+                    version: { type: 'string' },
+                  },
+                },
+                cvss_v3: {
+                  additionalProperties: true,
+                  properties: {
+                    version: { type: 'string' },
+                  },
+                },
+                cvss_v4: {
+                  additionalProperties: true,
+                  properties: {
+                    version: { type: 'string' },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+})
+
+const validate = jtdAjv.compile(inputSchema)
+
 /**
  *
  * @param {unknown} doc
  */
 export default function mandatoryTest_6_1_7(doc) {
+  const ctx = {
+    errors:
+      /** @type {Array<{ instancePath: string; message: string }>} */ ([]),
+    isValid: true,
+  }
+
   /** @type {Array<{ message: string; instancePath: string }>} */
   const errors = []
   let isValid = true
 
-  // 6.1.7 Multiple Scores with same Version per Product
-  if (preconditionFor_6_1_7_Matches(doc)) {
-    doc.vulnerabilities.forEach((vulnerability, vulnerabilityIndex) => {
-      /** @type {Map<string, Set<string>>} */
-      const cvssVersionsByProductName = new Map()
-
-      vulnerability.metrics?.forEach((metric, scoreIndex) => {
-        metric.products?.forEach((product, productIndex) => {
-          const versionSet = cvssVersionsByProductName.get(product) ?? new Set()
-          cvssVersionsByProductName.set(product, versionSet)
-
-          if (
-            (metric.content?.cvss_v2?.version !== undefined &&
-              versionSet.has(metric.content?.cvss_v2.version)) ||
-            (metric.content?.cvss_v3?.version !== undefined &&
-              versionSet.has(metric.content?.cvss_v3.version))||
-              (metric.content?.cvss_v4?.version !== undefined &&
-                  versionSet.has(metric.content?.cvss_v4.version))
-          ) {
-            isValid = false
-            errors.push({
-              message: `product is already included in these cvss-versions: ${Array.from(
-                versionSet.keys()
-              ).join(', ')}`,
-              instancePath: `/vulnerabilities/${vulnerabilityIndex}/metrics/${scoreIndex}/products/${productIndex}`,
-            })
-          }
-          if (metric.content?.cvss_v2?.version !== undefined) {
-            versionSet.add(metric.content?.cvss_v2.version)
-          }
-          if (metric.content?.cvss_v3?.version !== undefined) {
-            versionSet.add(metric.content?.cvss_v3.version)
-          }
-          if (metric.content?.cvss_v4?.version !== undefined) {
-            versionSet.add(metric.content?.cvss_v4.version)
-          }
-        })
-      })
-    })
+  if (!validate(doc)) {
+    return ctx
   }
 
-  return { errors, isValid }
-}
+  // 6.1.7 Multiple Scores with same Version per Product
+  /** @type {Array<any>} */
+  const vulnerabilities = doc.vulnerabilities
+  vulnerabilities.forEach((vulnerability, vulnerabilityIndex) => {
+    /** @type {Map<string, Set<string>>} */
+    const cvssVersionsByProductName = new Map()
 
-/**
- * @param {unknown} rawDoc
- * @returns {rawDoc is {
- *    vulnerabilities: Array<{
- *      metrics?: Array<{
- *        products?: string[]
- *        content?: {
- *          cvss_v3?: { version?: string }
- *          cvss_v2?: { version?: string }
- *          cvss_v4?: { version?: string }
- *          }
- *      }>
- *    }>
- *  }}
- */
-const preconditionFor_6_1_7_Matches = (rawDoc) => {
-  if (typeof rawDoc !== 'object' || !rawDoc) return false
-  /** @type {{ vulnerabilities?: unknown }} */
-  const doc = rawDoc
-  return (
-    Array.isArray(doc.vulnerabilities) &&
-    doc.vulnerabilities.every(
-      (vulnerability) =>
-        (Array.isArray(vulnerability.metrics) &&
-          vulnerability.metrics.every(
-            (
-              /** @type {{ products?: unknown; content?: {cvss_v2?: any; cvss_v3?: any; cvss_v4?: any }}} */ metric
-            ) =>
-              Array.isArray(metric.products) &&
-              metric.products.every((product) => typeof product === 'string') &&
-              ((metric.content?.cvss_v2 &&
-                (typeof metric.content?.cvss_v2.version === 'string' ||
-                  metric.content?.cvss_v2.version === undefined)) ||
-                metric.content?.cvss_v2 === undefined) &&
-              ((metric.content?.cvss_v3 &&
-                (typeof metric.content?.cvss_v3.version === 'string' ||
-                  metric.content?.cvss_v3.version === undefined)) ||
-                metric.content?.cvss_v3 === undefined) &&
-                ((metric.content?.cvss_v4 &&
-                        (typeof metric.content?.cvss_v4.version === 'string' ||
-                            metric.content?.cvss_v4.version === undefined)) ||
-                    metric.content?.cvss_v4 === undefined)
-          )) ||
-        vulnerability.metrics === undefined
-    )
-  )
+    /** @type {Array<any>} */
+    const metrics = vulnerability.metrics
+    metrics?.forEach((metric, scoreIndex) => {
+      /** @type {Array<any>} */
+      const products = metric.products
+      products?.forEach((product, productIndex) => {
+        const versionSet = cvssVersionsByProductName.get(product) ?? new Set()
+        cvssVersionsByProductName.set(product, versionSet)
+
+        if (
+          (metric.content?.cvss_v2?.version !== undefined &&
+            versionSet.has(metric.content?.cvss_v2.version)) ||
+          (metric.content?.cvss_v3?.version !== undefined &&
+            versionSet.has(metric.content?.cvss_v3.version)) ||
+          (metric.content?.cvss_v4?.version !== undefined &&
+            versionSet.has(metric.content?.cvss_v4.version))
+        ) {
+          isValid = false
+          errors.push({
+            message: `product is already included in these cvss-versions: ${Array.from(
+              versionSet.keys()
+            ).join(', ')}`,
+            instancePath: `/vulnerabilities/${vulnerabilityIndex}/metrics/${scoreIndex}/products/${productIndex}`,
+          })
+        }
+        if (metric.content?.cvss_v2?.version !== undefined) {
+          versionSet.add(metric.content?.cvss_v2.version)
+        }
+        if (metric.content?.cvss_v3?.version !== undefined) {
+          versionSet.add(metric.content?.cvss_v3.version)
+        }
+        if (metric.content?.cvss_v4?.version !== undefined) {
+          versionSet.add(metric.content?.cvss_v4.version)
+        }
+      })
+    })
+  })
+
+  return { errors, isValid }
 }
