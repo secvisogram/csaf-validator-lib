1a6f80a71d6c5d4380af34dbb4f27d894291ef80
chirschenberger <christopher.hirschenberger@exxcellent.de>
2025-03-27 13:36:45 +0100

    style: reformat changed files with prettier

--- tests/csaf_2_1/optionalTest_6_2_3.js
+++ tests/csaf_2_1/optionalTest_6_2_3.js
@@ -3,6 +3,9 @@ import { optionalTest_6_2_3 } from '../../csaf_2_1/optionalTests.js'
 
 describe('optionalTest_6_2_3', function () {
   it('only runs on relevant documents', function () {
-    assert.equal(optionalTest_6_2_3({ vulnerabilities: 'mydoc' }).warnings.length, 0)
+    assert.equal(
+      optionalTest_6_2_3({ vulnerabilities: 'mydoc' }).warnings.length,
+      0
+    )
   })
 })
