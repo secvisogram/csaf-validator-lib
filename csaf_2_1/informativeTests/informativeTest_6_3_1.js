b3b6f47cf2c7c38f9bbe3038b8c21ed5bf050be8
rschneider <97682836+rainer-exxcellent@users.noreply.github.com>
2025-04-25 12:23:00 +0200

    feat(CSAF2.1): #199 test 6.3.1 for CSAF 2.1 - cover second scoring object

--- csaf_2_1/informativeTests/informativeTest_6_3_1.js
+++ csaf_2_1/informativeTests/informativeTest_6_3_1.js
@@ -2,6 +2,16 @@ import Ajv from 'ajv/dist/jtd.js'
 
 const ajv = new Ajv()
 
+/**
+ * @typedef {{ cvss_v2: {version: string}, cvss_v3: {version: string}, cvss_v4: {version: string}}} MetricContent
+ */
+
+/**
+ * @typedef {Object} Metric
+ * @property {MetricContent} content
+ * @property {Array<string>} products
+ */
+
 const inputSchema = /** @type {const} */ ({
   additionalProperties: true,
   properties: {
@@ -13,21 +23,32 @@ const inputSchema = /** @type {const} */ ({
           metrics: {
             elements: {
               additionalProperties: true,
+              properties: {
+                products: {
+                  elements: {},
+                },
+              },
               optionalProperties: {
                 content: {
                   additionalProperties: true,
                   optionalProperties: {
                     cvss_v2: {
                       additionalProperties: true,
-                      properties: {},
+                      properties: {
+                        version: { type: 'string' },
+                      },
                     },
                     cvss_v3: {
                       additionalProperties: true,
-                      properties: {},
+                      properties: {
+                        version: { type: 'string' },
+                      },
                     },
                     cvss_v4: {
                       additionalProperties: true,
-                      properties: {},
+                      properties: {
+                        version: { type: 'string' },
+                      },
                     },
                   },
                 },
@@ -55,16 +76,47 @@ export function informativeTest_6_3_1(doc) {
     return ctx
   }
 
-  doc.vulnerabilities.forEach((vulnerability, vulnerabilityIndex) => {
-    vulnerability.metrics?.forEach((metric, metricIndex) => {
-      if (
-        metric.content?.cvss_v2 &&
-        !metric.content?.cvss_v3 &&
-        !metric.content?.cvss_v4
-      ) {
+  /**
+   * @param {Metric} metric
+   * @param {Set<string>} versionSet
+   */
+  function addVersionsInMetricToSet(metric, versionSet) {
+    if (metric.content?.cvss_v2?.version !== undefined) {
+      versionSet.add(metric.content?.cvss_v2.version)
+    }
+    if (metric.content?.cvss_v3?.version !== undefined) {
+      versionSet.add(metric.content?.cvss_v3.version)
+    }
+    if (metric.content?.cvss_v4?.version !== undefined) {
+      versionSet.add(metric.content?.cvss_v4.version)
+    }
+  }
+
+  /** @type {Array<any>} */
+  const vulnerabilities = doc.vulnerabilities
+
+  vulnerabilities.forEach((vulnerability, vulnerabilityIndex) => {
+    /** @type {Map<string, Set<string>>} */
+    const cvssVersionsByProduct = new Map()
+    const metricIndexByProduct = new Map()
+    /** @type {Array<any>} */
+    const metrics = vulnerability.metrics
+    metrics?.forEach((metric, metricIndex) => {
+      /** @type {Array<any>} */
+      const products = metric.products
+      products.forEach((product) => {
+        const versionSet = cvssVersionsByProduct.get(product) ?? new Set()
+        cvssVersionsByProduct.set(product, versionSet)
+        metricIndexByProduct.set(product, metricIndex)
+        addVersionsInMetricToSet(metric, versionSet)
+      })
+    })
+    cvssVersionsByProduct.forEach((value, product) => {
+      if (value.size === 1 && value.values().next().value === '2.0') {
+        const metricIndex = metricIndexByProduct.get(product)
         ctx.infos.push({
           instancePath: `/vulnerabilities/${vulnerabilityIndex}/metrics/${metricIndex}`,
-          message: 'use of cvss v2 as the only scoring system',
+          message: `use of cvss v2 as the only scoring system for product ${product}`,
         })
       }
     })
