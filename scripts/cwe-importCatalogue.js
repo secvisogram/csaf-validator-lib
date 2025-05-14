66c3c42bc05a198d60884b5077a3167440f14587
chirschenberger <christopher.hirschenberger@exxcellent.de>
2025-05-14 16:34:05 +0200

    feat: refactor script

--- scripts/cwe-importCatalogue.js
+++ scripts/cwe-importCatalogue.js
@@ -54,7 +54,7 @@ const json = {
   weaknesses: fileXML.Weaknesses.Weakness.map(
     (/** @type {Weakness} */ weakness) => {
       return {
-        id: `CWEe-${weakness.ID}`,
+        id: `CWE-${weakness.ID}`,
         name: weakness.Name,
         status: weakness.Status,
         /* Please note that the Usage property only exists in cwe v4.12 and newer. */
