d005a122de38fdeb0a45af3fb5ed93790ce16a08
chirschenberger <christopher.hirschenberger@exxcellent.de>
2025-05-14 13:24:01 +0200

    feat: adapt script to use null for the usage property if its missing

--- scripts/cwe-importCatalogue.js
+++ scripts/cwe-importCatalogue.js
@@ -48,10 +48,13 @@ for (const version of cwec) {
    * */
   let firstCweVersionReleaseDate = undefined
   if (version === '1.0') {
-    console.log('here')
     firstCweVersionReleaseDate = '2008-09-09'
   }
   const json = {
+    /*
+     * For v3.0 and newer, the respective property is named "Date", for older versions its "Catalog_Date".
+     * For v1.0 its missing, use firstCweVersionReleaseDate (see above).
+     * */
     date: fileXML.Date || fileXML.Catalog_Date || firstCweVersionReleaseDate,
     weaknesses: fileXML.Weaknesses.Weakness.map(
       (/** @type {Weakness} */ weakness) => {
@@ -59,8 +62,13 @@ for (const version of cwec) {
           id: `CWE-${weakness.ID}`,
           name: weakness.Name,
           status: weakness.Status,
-          // Please note that the Usage property only exists in cwe version 4.12 and newer
-          usage: weakness.Mapping_Notes?.Usage,
+          /*
+           * Please note that the Usage property only exists in cwe v4.12 and newer.
+           * Nevertheless this field must not be missing in the CWE catalogue for earlier versions,
+           * since this would lead to errors in the respective optional tests.
+           * Therefore the value null is assigned for the Usage property for earlier versions.
+           */
+          usage: weakness.Mapping_Notes?.Usage || null,
         }
       }
     ),
