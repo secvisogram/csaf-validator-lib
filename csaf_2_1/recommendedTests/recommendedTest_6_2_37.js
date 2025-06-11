3013838821294a6723a9e2224ea3e6a607af5268
chirschenberger <christopher.hirschenberger@exxcellent.de>
2025-06-11 17:30:46 +0200

    refactor: rename optional test 6.2.37 to recommended test

--- csaf_2_1/recommendedTests/recommendedTest_6_2_37.js
+++ csaf_2_1/recommendedTests/recommendedTest_6_2_37.js
@@ -38,11 +38,11 @@ const inputSchema = /** @type {const} */ ({
 const validate = ajv.compile(inputSchema)
 
 /**
- * This implements the optional test 6.2.37 of the CSAF 2.1 standard.
+ * This implements the recommended test 6.2.37 of the CSAF 2.1 standard.
  *
  * @param {any} doc
  */
-export function optionalTest_6_2_37(doc) {
+export function recommendedTest_6_2_37(doc) {
   /** @type {Array<{ message: string; instancePath: string }>} */
   const warnings = []
   const context = { warnings }
--- tests/csaf_2_1/optionalTest_6_2_37.js
+++ tests/csaf_2_1/optionalTest_6_2_37.js
@@ -1,10 +1,10 @@
 import assert from 'node:assert'
-import { optionalTest_6_2_37 } from '../../csaf_2_1/optionalTests.js'
+import { recommendedTest_6_2_37 } from '../../csaf_2_1/recommendedTests.js'
 
-describe('optionalTest_6_2_37', function () {
+describe('recommendedTest_6_2_37', function () {
   it('only runs on relevant documents', function () {
     assert.equal(
-      optionalTest_6_2_37({ vulnerabilities: 'mydoc' }).warnings.length,
+      recommendedTest_6_2_37({ vulnerabilities: 'mydoc' }).warnings.length,
       0
     )
   })
