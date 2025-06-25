e3fa899c2c7b55ee8b95a460134748e17c94a613
chirschenberger <christopher.hirschenberger@exxcellent.de>
2025-06-25 14:25:23 +0200

    feat: add test for input schema

--- tests/csaf_2_1/mandatoryTest_6_1_47.js
+++ tests/csaf_2_1/mandatoryTest_6_1_47.js
@@ -1,8 +1,44 @@
 import assert from 'node:assert/strict'
 import { mandatoryTest_6_1_47 } from '../../csaf_2_1/mandatoryTests/mandatoryTest_6_1_47.js'
+import { expect } from 'chai'
+
+const validInputSchemaTestWithEmptyVulnerability6_1_47 = {
+  vulnerabilities: [
+    {}, // even this vulnerability is empty, the test should not fail due to the inputSchema
+    {
+      cve: 'CVE-1900-0001',
+      metrics: [
+        {
+          content: {
+            ssvc_v1: {
+              id: 'CVE-1900-0001',
+              schemaVersion: '1-0-1',
+              selections: [
+                {
+                  name: 'Exploitation',
+                  namespace: 'ssvc',
+                  values: ['None'],
+                  version: '1.1.0',
+                },
+              ],
+              timestamp: '2024-01-24T10:00:00.000Z',
+            },
+          },
+          products: ['CSAFPID-9080700'],
+        },
+      ],
+    },
+  ],
+}
 
 describe('mandatoryTest_6_1_47', function () {
   it('only runs on relevant documents', function () {
     assert.equal(mandatoryTest_6_1_47({ document: 'mydoc' }).isValid, true)
+  })
+  it('test input schema with empty json object in vulnerabilities', async function () {
+    const result = mandatoryTest_6_1_47(
+      validInputSchemaTestWithEmptyVulnerability6_1_47
+    )
+    expect(result.errors.length).to.eq(0)
   })
 })
