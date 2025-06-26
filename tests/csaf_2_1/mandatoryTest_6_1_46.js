715c93443a8a29104ed2631a1e18cda6ea270602
chirschenberger <christopher.hirschenberger@exxcellent.de>
2025-06-26 12:10:26 +0200

    feat: change inputSchema test from valid to failing example

--- tests/csaf_2_1/mandatoryTest_6_1_46.js
+++ tests/csaf_2_1/mandatoryTest_6_1_46.js
@@ -2,24 +2,17 @@ import assert from 'node:assert/strict'
 import { mandatoryTest_6_1_46 } from '../../csaf_2_1/mandatoryTests/mandatoryTest_6_1_46.js'
 import { expect } from 'chai'
 
-const validInputSchemaTestWithEmptyVulnerability6_1_46 = {
+const failingInputSchemaTestWithEmptyVulnerability6_1_46 = {
   vulnerabilities: [
     {}, // even this vulnerability is empty, the test should not fail due to the inputSchema
     {
+      cve: 'CVE-1900-0001',
       metrics: [
         {
           content: {
             ssvc_v1: {
               id: 'CVE-1900-0001',
               schemaVersion: '1-0-1',
-              selections: [
-                {
-                  name: 'Mission Impact',
-                  namespace: 'ssvc',
-                  values: ['None'],
-                  version: '1.0.0',
-                },
-              ],
               timestamp: '2024-01-24T10:00:00.000Z',
             },
           },
@@ -36,8 +29,8 @@ describe('mandatoryTest_6_1_46', function () {
   })
   it('test input schema with empty json object in vulnerabilities', async function () {
     const result = mandatoryTest_6_1_46(
-      validInputSchemaTestWithEmptyVulnerability6_1_46
+      failingInputSchemaTestWithEmptyVulnerability6_1_46
     )
-    expect(result.errors.length).to.eq(0)
+    expect(result.errors.length).to.eq(1)
   })
 })
