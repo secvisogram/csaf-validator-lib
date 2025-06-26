d2e3a1f4459861beefc3e3f31093d1dcd2a07f8c
chirschenberger <christopher.hirschenberger@exxcellent.de>
2025-06-26 13:07:28 +0200

    feat: change some properties to optionalProperties and adapt inputSchema test accordingly

--- tests/csaf_2_1/mandatoryTest_6_1_47.js
+++ tests/csaf_2_1/mandatoryTest_6_1_47.js
@@ -2,16 +2,17 @@ import assert from 'node:assert/strict'
 import { mandatoryTest_6_1_47 } from '../../csaf_2_1/mandatoryTests/mandatoryTest_6_1_47.js'
 import { expect } from 'chai'
 
-const validInputSchemaTestWithEmptyVulnerability6_1_47 = {
+const failingInputSchemaTestWithEmptyVulnerability6_1_47 = {
+  document: {},
   vulnerabilities: [
-    {}, // even this vulnerability is empty, the test should not fail due to the inputSchema
+    {}, // even this vulnerability is empty, the test should run
     {
       cve: 'CVE-1900-0001',
       metrics: [
         {
           content: {
             ssvc_v1: {
-              id: 'CVE-1900-0001',
+              id: 'CVE-1900-0002',
               schemaVersion: '1-0-1',
               selections: [
                 {
@@ -37,8 +38,8 @@ describe('mandatoryTest_6_1_47', function () {
   })
   it('test input schema with empty json object in vulnerabilities', async function () {
     const result = mandatoryTest_6_1_47(
-      validInputSchemaTestWithEmptyVulnerability6_1_47
+      failingInputSchemaTestWithEmptyVulnerability6_1_47
     )
-    expect(result.errors.length).to.eq(0)
+    expect(result.errors.length).to.eq(1)
   })
 })
