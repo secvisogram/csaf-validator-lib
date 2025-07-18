4209c79837999de3d3e9dba37422e9fbbd42e9c1
rschneider <97682836+rainer-exxcellent@users.noreply.github.com>
2025-07-18 10:07:39 +0200

    feat(CSAF2.1): #373 add mandatory test 6.1.44 - add comment to containMultipleUnescapedStars

--- csaf_2_1/mandatoryTests/mandatoryTest_6_1_44.js
+++ csaf_2_1/mandatoryTests/mandatoryTest_6_1_44.js
@@ -78,13 +78,19 @@ const validate = ajv.compile(inputSchema)
  */
 
 /**
+ * Checks if the `stringToCheck` includes more than one unescaped `*` character. A `*` character
+ * can be escaped by prefixing it with a backslash (`\`).
  *
  * @param {string} stringToCheck
  * @return {boolean}
  */
 export function containMultipleUnescapedStars(stringToCheck) {
   const regex = /\*/g
-  return (stringToCheck.replace(/\\\*/g, '').match(regex)?.length ?? 0) > 1
+  return (
+    (stringToCheck
+      .replace(/\\\*/g, '') // remove escaped '*'
+      .match(regex)?.length ?? 0) > 1 // check if there is more than 1 unescaped '*'
+  )
 }
 
 /**
