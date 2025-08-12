453398be0b5a4a28500c64b1d26b2bd1d339df38
chirschenberger <christopher.hirschenberger@exxcellent.de>
2025-08-12 15:32:21 +0200

    feat: adapt recommended test 6.2.21

--- tests/csaf_2_1/recommendedTest_6_2_21.js
+++ tests/csaf_2_1/recommendedTest_6_2_21.js
@@ -32,4 +32,48 @@ describe('recommendedTest_6_2_21', function () {
       1
     )
   })
+  /**
+   * Tests if in the warnings message the right revision_history items are referenced even the revision_history
+   * contains items where the "date" property is undefined
+   * */
+  it('warnings message references right revision history items in case of undefined dates', function () {
+    assert.deepEqual(
+      recommendedTest_6_2_21({
+        document: {
+          tracking: {
+            revision_history: [
+              {
+                number: '4.0.0',
+                summary: 'Fourth version.',
+              },
+              {
+                date: '2024-01-21T10:00:00.000Z',
+                number: '2.0.0',
+                summary: 'Second version.',
+              },
+              {
+                date: '2025-02-28T14:23:59.000Z',
+                number: '3.0.0',
+                summary: 'Third version.',
+              },
+              {},
+              {},
+              {
+                date: '2024-01-21T10:00:00.000Z',
+                number: '1.0.0',
+                summary: 'Initial version.',
+              },
+            ],
+          },
+        },
+      }).warnings,
+      [
+        {
+          instancePath: '/document/tracking/revision_history/2/date',
+          message:
+            'the timestamps of the revision history items with version number 2.0.0 and 1.0.0 are equal',
+        },
+      ]
+    )
+  })
 })
