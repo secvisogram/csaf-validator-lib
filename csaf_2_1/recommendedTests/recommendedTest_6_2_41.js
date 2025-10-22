b113ef6188cb42fb1e59861d519cabd555f197ba
bendo-eXX <benjamin.dorn@exxcellent.de>
2025-10-22 17:05:46 +0200

    Revert "feat(CSAF2.1): remove double checks"
    This reverts commit 8848bb88507361f5b101dc5c7e90ba8a7d713cfe.

--- csaf_2_1/recommendedTests/recommendedTest_6_2_41.js
+++ csaf_2_1/recommendedTests/recommendedTest_6_2_41.js
@@ -94,13 +94,19 @@ export function recommendedTest_6_2_41(doc) {
       .map((m) => m.content?.epss)
       .filter((item) => item?.timestamp != null)
       .sort((a, z) => {
+        if (!a || !z) return 0
         return compareZonedDateTimes(
           /** @type {string} */ (z.timestamp),
           /** @type {string} */ (a.timestamp)
         )
       })[0]
 
-    if (!newestEpss || !newestEpss.timestamp) {
+    if (
+      !newestEpss ||
+      !newestEpss.timestamp ||
+      !newestRevisionHistoryItem ||
+      !newestRevisionHistoryItem.date
+    ) {
       return context
     }
 
