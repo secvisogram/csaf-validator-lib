dfde4d4c3e311fa969158b30a9451cfae99e6805
bendo-eXX <benjamin.dorn@exxcellent.de>
2026-05-07 13:04:53 +0200

    fix(CSAF2.1): instancePath and any

--- csaf_2_1/mandatoryTests/mandatoryTest_6_1_53.js
+++ csaf_2_1/mandatoryTests/mandatoryTest_6_1_53.js
@@ -1,4 +1,4 @@
-import Ajv from 'ajv/dist/jtd.js'
+import { Ajv } from 'ajv/dist/jtd.js'
 import { compareZonedDateTimes } from '../dateHelper.js'
 
 const ajv = new Ajv()
@@ -36,7 +36,7 @@ const validate = ajv.compile(inputSchema)
 /**
  * This implements the mandatory test 6.1.53 of the CSAF 2.1 standard.
  *
- * @param {any} doc
+ * @param {unknown} doc
  */
 export function mandatoryTest_6_1_53(doc) {
   /*
@@ -67,7 +67,7 @@ export function mandatoryTest_6_1_53(doc) {
       ) {
         ctx.isValid = false
         ctx.errors.push({
-          instancePath: `/vulnerabilities/${vulnerabilityIndex}/first_known_exploitation_dates/${Index}`,
+          instancePath: `/vulnerabilities/${vulnerabilityIndex}/first_known_exploitation_dates/${Index}/exploitation_date`,
           message:
             'the "exploitation_date" is newer than the "date" which states when that information was last updated',
         })
