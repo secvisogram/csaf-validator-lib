01d785aa26110355f0fc5697c421cb412ee80237
rschneider <97682836+rainer-exxcellent@users.noreply.github.com>
2026-06-03 07:19:02 +0200

    feat(CSAF2.1): #362 add mandatory test 6.1.49 - changed ssvc_v1 to ssvc_2, add some test for edge cases

--- tests/csaf_2_1/mandatoryTest_6_1_49.js
+++ tests/csaf_2_1/mandatoryTest_6_1_49.js
@@ -2,52 +2,195 @@ import assert from 'node:assert/strict'
 import { mandatoryTest_6_1_49 } from '../../csaf_2_1/mandatoryTests/mandatoryTest_6_1_49.js'
 import { expect } from 'chai'
 
-const failingInputSchemaTestWithEmptyVulnerability6_1_49 = {
-  document: {
-    tracking: {
-      revision_history: [
-        {
-          date: '2024-01-24T10:00:00.000Z',
-        },
-      ],
-      status: 'final',
-    },
-  },
-  vulnerabilities: [
-    {}, // even this vulnerability is empty, the test should not fail due to the inputSchema
-    {
-      cve: 'CVE-1900-0001',
-      metrics: [
-        {
-          content: {
-            ssvc_v1: {
-              id: 'CVE-1900-0001',
-              schemaVersion: '1-0-1',
-              selections: [
-                {
-                  name: 'Exploitation',
-                  namespace: 'ssvc',
-                  values: ['Active'],
-                  version: '1.1.0',
-                },
-              ],
-              timestamp: '2024-07-13T10:00:00.000Z',
-            },
-          },
-        },
-      ],
-    },
-  ],
-}
-
 describe('mandatoryTest_6_1_49', function () {
   it('only runs on relevant documents', function () {
     assert.equal(mandatoryTest_6_1_49({ document: 'mydoc' }).isValid, true)
   })
+
   it('test input schema with empty json object in vulnerabilities', async function () {
+    const failingInputSchemaTestWithEmptyVulnerability = {
+      document: {
+        tracking: {
+          revision_history: [
+            {
+              date: '2024-01-24T10:00:00.000Z',
+            },
+          ],
+          status: 'final',
+        },
+      },
+      vulnerabilities: [
+        {}, // even this vulnerability is empty, the test should not fail due to the inputSchema
+        {
+          cve: 'CVE-1900-0001',
+          metrics: [
+            {
+              content: {
+                ssvc_v2: {
+                  id: 'CVE-1900-0001',
+                  schemaVersion: '1-0-1',
+                  timestamp: '2024-07-13T10:00:00.000Z',
+                },
+              },
+            },
+          ],
+        },
+      ],
+    }
+
     const result = mandatoryTest_6_1_49(
-      failingInputSchemaTestWithEmptyVulnerability6_1_49
+      failingInputSchemaTestWithEmptyVulnerability
     )
     expect(result.errors.length).to.eq(1)
+  })
+  it('test input schema with empty/invalid dates in revision_history', async function () {
+    const failingSchemaTestWithEmptyDates = {
+      document: {
+        tracking: {
+          revision_history: [
+            {
+              number: '1',
+            },
+            {
+              date: '',
+              number: '2',
+            },
+            {
+              date: '1.3.45',
+              number: '2',
+            },
+          ],
+          status: 'final',
+        },
+      },
+      vulnerabilities: [
+        {
+          metrics: [
+            {
+              content: {
+                ssvc_v2: {
+                  timestamp: '2024-07-13T10:00:00.000Z',
+                },
+              },
+            },
+          ],
+        },
+      ],
+    }
+
+    const result = mandatoryTest_6_1_49(failingSchemaTestWithEmptyDates)
+    expect(result.errors.length).to.eq(0)
+  })
+
+  it('test input schema with empty/invalid dates in revision_history', async function () {
+    const failingSchemaTestWithEmptyDates = {
+      document: {
+        tracking: {
+          revision_history: [
+            {
+              number: '1',
+            },
+            {
+              date: '',
+              number: '2',
+            },
+            {
+              date: '1.3.45',
+              number: '2',
+            },
+          ],
+          status: 'final',
+        },
+      },
+      vulnerabilities: [
+        {
+          metrics: [
+            {
+              content: {
+                ssvc_v2: {
+                  timestamp: '2024-07-13T10:00:00.000Z',
+                },
+              },
+            },
+          ],
+        },
+      ],
+    }
+
+    const result = mandatoryTest_6_1_49(failingSchemaTestWithEmptyDates)
+    expect(result.errors.length).to.eq(0)
+  })
+
+  it('test input schema with empty json object in ssvc_2', async function () {
+    const failingInputSchemaTestWithEmptyVulnerability = {
+      document: {
+        tracking: {
+          revision_history: [
+            {
+              date: '2024-01-24T10:00:00.000Z',
+            },
+          ],
+          status: 'final',
+        },
+      },
+      vulnerabilities: [
+        {
+          cve: 'CVE-1900-0001',
+          metrics: [
+            {
+              content: {
+                epss: {}, // even this ssvc_v2 is here empty, the test should not fail due to the inputSchema
+              },
+            },
+            {
+              content: {
+                ssvc_v2: {
+                  id: 'CVE-1900-0001',
+                  schemaVersion: '1-0-1',
+                  timestamp: '2024-07-13T10:00:00.000Z',
+                },
+              },
+            },
+          ],
+        },
+      ],
+    }
+
+    const result = mandatoryTest_6_1_49(
+      failingInputSchemaTestWithEmptyVulnerability
+    )
+    expect(result.errors.length).to.eq(1)
+  })
+
+  it('test input schema with empty revision history', async function () {
+    const failingInputSchemaTestWithEmptyVulnerability = {
+      document: {
+        tracking: {
+          revision_history: [],
+          status: 'final',
+        },
+      },
+      vulnerabilities: [
+        {
+          cve: 'CVE-1900-0001',
+          metrics: [
+            {
+              content: {
+                ssvc_v2: {
+                  id: 'CVE-1900-0001',
+                  schemaVersion: '1-0-1',
+                  timestamp: '2024-07-13T10:00:00.000Z',
+                },
+              },
+            },
+          ],
+        },
+      ],
+    }
+
+    const result = mandatoryTest_6_1_49(
+      failingInputSchemaTestWithEmptyVulnerability
+    )
+    expect(result.errors.length).to.eq(0)
   })
 })
