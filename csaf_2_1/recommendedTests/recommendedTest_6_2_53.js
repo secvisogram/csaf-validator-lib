4c02f08f3582b94220aed2e1badb42f972a59ea9
bendo-eXX <benjamin.dorn@exxcellent.de>
2026-08-20 08:51:39 +0200

    Apply suggestions from code review
    Co-authored-by: Dominik Burgdörfer <db@burgdoerfer.com>

--- csaf_2_1/recommendedTests/recommendedTest_6_2_53.js
+++ csaf_2_1/recommendedTests/recommendedTest_6_2_53.js
@@ -3,16 +3,11 @@ import { entries } from '../../rvisc.js'
 
 const ajv = new Ajv()
 
-/** @type {Array<{ system_name: string; common_name: string; text_pattern: RegExp }>} */
-const registeredIdSystems = entries.map(
-  (
-    /** @type {{ system_name: string; common_name: string; text_pattern: string }} */ entry
-  ) => ({
-    system_name: entry.system_name,
-    common_name: entry.common_name,
-    text_pattern: new RegExp(entry.text_pattern),
-  })
-)
+const registeredIdSystems = entries.map((entry) => ({
+  system_name: entry.system_name,
+  common_name: entry.common_name,
+  text_pattern: new RegExp(entry.text_pattern),
+}))
 
 /*
   This is the jtd schema that needs to match the input document so that the
@@ -45,9 +40,6 @@ const inputSchema = /** @type {const} */ ({
 
 const validate = ajv.compile(inputSchema)
 
-/** @typedef {import('ajv/dist/jtd.js').JTDDataType<typeof inputSchema>} InputSchema */
-/** @typedef {InputSchema['vulnerabilities'][number]} Vulnerability */
-
 /**
  * This implements the recommended test 6.2.53 of the CSAF 2.1 standard.
  *
@@ -63,7 +55,6 @@ export function recommendedTest_6_2_53(doc) {
     return ctx
   }
 
-  /** @type {Array<Vulnerability>} */
   const vulnerabilities = doc.vulnerabilities
   vulnerabilities.forEach((vulnerability, vulnIndex) => {
     const ids = vulnerability.ids ?? []
