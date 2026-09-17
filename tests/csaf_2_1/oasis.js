43dae77cfc165ac249be4a2aaa7c4ceb0bcf211e
Philipp Backes <philipp.backes@exxcellent.de>
2026-09-17 18:06:32 +0200

    feat(CSAF2.1): adapt mandatory test 6.1.24 to match new schema

--- tests/csaf_2_1/oasis.js
+++ tests/csaf_2_1/oasis.js
@@ -8,7 +8,6 @@ import isBrowserRuntime from '../shared/isBrowserRuntime.js'
  * Once all tests are implemented for CSAF 2.1 this should be deleted.
  */
 const excluded = [
-  '6.1.24',
   '6.1.26',
   '6.1.27.2',
   '6.1.27.8',
