ed7fdfdf4b0002122572f1354a51cc0474e62a6c
bendo-eXX <benjamin.dorn@exxcellent.de>
2026-09-25 15:13:10 +0200

    feat(csaf2.1): add check for hostname

--- csaf_2_1/recommendedTests/recommendedTest_6_2_11.js
+++ csaf_2_1/recommendedTests/recommendedTest_6_2_11.js
@@ -1,4 +1,5 @@
 import { Ajv } from 'ajv/dist/jtd.js'
+import { isCanonicalUrl } from '#lib/shared/urlHelper.js'
 
 const ajv = new Ajv()
 
@@ -36,7 +37,11 @@ const validateInput = ajv.compile(inputSchema)
  */
 
 /**
- * Test for the optional test 6.2.11
+ * This implements the recommended test 6.2.11 of the CSAF 2.1 standard.
+ *
+ * It MUST be tested that the document contains at least one reference with category "self"
+ * and a valid URL that fulfills the requirements of a valid filename for a CSAF document.
+ *
  * @param {unknown} doc
  */
 export function recommendedTest_6_2_11(doc) {
@@ -49,13 +54,6 @@ export function recommendedTest_6_2_11(doc) {
     return ctx
   }
 
-  const trackingId = doc.document.tracking.id
-  if (!trackingId) {
-    return ctx
-  }
-
-  const filename = transformTrackingIdToFilename(trackingId)
-
   /** @type {Array<Reference & { index: number }>} */
   const selfReferences =
     doc.document.references
@@ -66,46 +64,26 @@ export function recommendedTest_6_2_11(doc) {
     return ctx
   }
 
-  const hasValidSelfReference = selfReferences.some((reference) => {
-    const url = reference.url || ''
-    return (
-      typeof url === 'string' &&
-      url.startsWith('https://') &&
-      url.endsWith(filename)
-    )
-  })
+  const trackingId = doc.document.tracking.id
+  if (!trackingId) {
+    return ctx
+  }
+
+  const hasValidSelfReference = selfReferences.some((reference) =>
+    isCanonicalUrl(reference, trackingId, true)
+  )
 
   if (hasValidSelfReference) {
     return ctx
   }
 
   selfReferences.forEach((reference) => {
-    const url = reference.url || ''
-    const referenceIndex = reference.index
-
-    if (
-      typeof url !== 'string' ||
-      !url.startsWith('https://') ||
-      !url.endsWith(filename)
-    ) {
-      ctx.warnings.push({
-        instancePath: `/document/references/${referenceIndex}/url`,
-        message:
-          'The reference category is "self", but the URL does not fulfill the requirements of a valid filename for a CSAF document.',
-      })
-    }
+    ctx.warnings.push({
+      instancePath: `/document/references/${reference.index}/url`,
+      message:
+        'The reference category is "self", but the URL does not fulfill the requirements of a valid filename for a CSAF document.',
+    })
   })
 
   return ctx
-}
-
-/** * Transforms a tracking ID into a filename.
- *
- * @param {string} trackingId
- * @returns {string} filename
- */
-function transformTrackingIdToFilename(trackingId) {
-  const lowerCase = trackingId.toLowerCase()
-  const replacedString = lowerCase.replace(/[^+\-a-z0-9]+/g, '_')
-  return replacedString + '.json'
 }
